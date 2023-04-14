1ab65c9c178a1eb4e7d68d5cd6873d64fd620e52
dannyafari <126033577+dannyafari@users.noreply.github.com>
2023-04-13 21:42:25 -0700

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -12,8 +12,4 @@ def call(String stageName){
      {
        sh "mvn clean deploy"
      }
-  else if ("${stageName}" == "Deploy to Tomcat"}
-     {
-     deploy adapters: [tomcat9(credentialsId: 'tomcatcredentials', path: '', url: 'http://107.23.108.142:8080/')], contextPath: null, onFailure: false, war: 'target/*war'
-     }
 }
